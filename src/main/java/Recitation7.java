b6e5b0c823ceaf0079efafb9d146abc34da91f31
malfaro21 <malfaro21@students.desu.edu>
2022-11-21 19:06:26 -0500

    Final

--- src/main/java/Recitation7.java
+++ src/main/java/Recitation7.java
@@ -8,27 +8,70 @@
 
 public class Recitation7
 {
+
     // define your constants after this comment
+    public static void main(String[] args){
+        System.out.println(Recitation7.leftRightTriangle(5));
+    }
 
 
+    private static String repString(char aChar, int numOfRep){
+        String str  = "";
+        for (int count = 0; count < numOfRep; count++){
+            str += aChar;
+        }
+        return str;
+    }
     public static String leftRightTriangle(int height)
     {
         String str = "";
-        //add your code here
+        for (int row = 0; row < height; row++)
+        {
+            str += repString('*', row+1) + "\n";
+        }
+
+
         return str;
     }
 
     public static String rightRightTriangle(int height)
     {
         String str = "";
-        //add your code here
+        int counter = 1;
+        for(int row = height - 1; row >= 0; row--){
+            for(int x = 0; x<row; x++){
+                str += ' ';
+            }
+            str += repString('*',counter) + "\n";
+            counter +=1;
+
+        }
         return str;
     }
 
     public static String circle(int radius)
     {
         String str = "";
-        //add your code here
+        for(int row = radius; row >=-(radius-1); row-- ){
+            int XandYproduct = (int) Math.sqrt(radius*radius-row*row);
+            int theNegAndPosAxis = XandYproduct *2;
+            int spacesOnBoth = (radius*2 - theNegAndPosAxis);
+            if(spacesOnBoth == radius*2){
+                str +="";
+            }
+            else {
+                for (int spaceOnOneSide =0;spaceOnOneSide< spacesOnBoth/2;spaceOnOneSide++ ){
+                    str+=" ";
+                }
+                for(int x = theNegAndPosAxis; x*2 > 0; x--){
+                    str += '*';
+                }
+                str+="\n";
+            }
+
+
+
+        }
         return str;
     }
 }
